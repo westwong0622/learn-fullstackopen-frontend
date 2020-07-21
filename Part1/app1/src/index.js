ba768de3a33bc6d1308aa7f03c1b32d96d2d2c38
West Wong <westwong.0622@gmail.com>
2020-07-21 13:59:18 +0800

    store click event to LR in allClicks

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
       <div>
-        {clicks.left}<button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>{clicks.right}
+        {left}<button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>{right}
+        <p>{allClicks.join(' ')}</p>
       </div>
     </div>
   )
