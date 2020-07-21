801b52a17f52b0d71ad1793a50c0ddbb4334434d
West Wong <westwong.0622@gmail.com>
2020-07-21 11:38:37 +0800

    onClick={handleClick}

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>Plus</button>
+    </div>
   )
 }
 
