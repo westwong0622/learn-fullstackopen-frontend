fe8a60bf8d0cc13cd00cbd56cf8a2d5e9106b1c3
West Wong <westwong.0622@gmail.com>
2020-07-21 14:00:52 +0800

    conditinal rendering

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -21,8 +21,24 @@ const App = () => {
       <div>
         {left}<button onClick={handleLeftClick}>left</button>
         <button onClick={handleRightClick}>right</button>{right}
-        <p>{allClicks.join(' ')}</p>
+        <History allClicks={allClicks} />
       </div>
+    </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
