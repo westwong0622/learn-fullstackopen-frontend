c4f0aabec44a0b961d1d5b6d1ea35f08400af101
West Wong <westwong.0622@gmail.com>
2020-07-21 15:09:23 +0800

    excercise 1.9

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -5,22 +5,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  return (
-    <div>
-      <h2>Give feedback</h2>
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
       <div>
-        <button onClick={() => setGood(good+1)}>Good</button>
-        <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
-        <button onClick={() => setBad(bad+1)}>Bad</button>
+        <h2>Give feedback</h2>
+        <div>
+          <button onClick={() => setGood(good+1)}>Good</button>
+          <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
+          <button onClick={() => setBad(bad+1)}>Bad</button>
+        </div>
+        <h3>Statistics</h3>
+        <h3>No feedback given</h3>
       </div>
-      <h3>Statistics</h3>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-    </div>
-  )
+    )
+  } 
+    return (
+      <div>
+        <h2>Give feedback</h2>
+        <div>
+          <button onClick={() => setGood(good+1)}>Good</button>
+          <button onClick={() => setNeutral(neutral+1)}>Neutral</button>
+          <button onClick={() => setBad(bad+1)}>Bad</button>
+        </div>
+        <h3>Statistics</h3>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+    )
+  
 }
 
 const Statistics = ( {good, neutral, bad} ) => {
