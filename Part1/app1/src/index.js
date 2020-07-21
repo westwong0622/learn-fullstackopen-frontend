72a821212b79522ce1d944397dfede5a2d2fd32b
West Wong <westwong.0622@gmail.com>
2020-07-21 14:31:52 +0800

    return function with diff para

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -4,14 +4,21 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const hello = () => {
-    const handler = () => console.log('hello world')
+  const hello = (who) => {
+    const handler = () => {
+      console.log('hello', who)
+    }
     return handler
   }
 
   return (
     <div>
-      {value}<button onClick={hello()}>button</button>
+      <div>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
+    </div>
     </div>
   )
 }
