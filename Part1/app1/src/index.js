157edf8a06a6627d183c5d9f9df7d820bdc0145f
West Wong <westwong.0622@gmail.com>
2020-07-21 13:39:04 +0800

    use components

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -4,20 +4,29 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne = () => {
-    setCounter(counter + 1)
-  }
-
-  const setToZero = () => {
-    setCounter(0)
-  }
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>Plus</button>
-      <button onClick={setToZero}>Reset</button>
+      <Display counter={counter}></Display>
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={setToZero} text='zero' />
+      <Button handleClick={decreaseByOne} text='minus' />
     </div>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
