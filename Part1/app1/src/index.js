9544d16eea7c6f18e7f3524d1397b12473faac55
West Wong <westwong.0622@gmail.com>
2020-07-21 13:55:28 +0800

    simplify it

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -4,21 +4,8 @@ import ReactDOM from 'react-dom';
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right: 0})
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
-
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
