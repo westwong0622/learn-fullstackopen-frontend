52a2d14fec1ef225bc36ab41263303ecc04922aa
West Wong <westwong.0622@gmail.com>
2020-07-27 22:33:29 +0800

    add app2

---
+++ Part2/app2/src/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
