55c48686290c6e0f48a6b8d5ffd3ea0dd1af87a3
West Wong <westwong.0622@gmail.com>
2020-07-27 18:13:41 +0800

    exercise 1.13 & 1.14

--- Part1/app1/src/index.js
+++ Part1/app1/src/index.js
@@ -3,15 +3,44 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [mostVoteOne, setMostVoteOne] = useState(0)
+  const [mostVoteOneIndex, setMostVoteOneIndex] = useState(0)
+
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0
+  })
 
   const randomAnecdote = () => {
-    setSelected(Math.floor(Math.random()*7))
+    setSelected(Math.floor(Math.random()*3))
+  }
+
+  const upvote = () => {
+    const copy = { ...points }
+    copy[selected] += 1
+    console.log(copy[selected])
+    if(copy[selected] > mostVoteOne) {
+      setMostVoteOne(copy[selected])
+      setMostVoteOneIndex(selected)
+    }
+    console.log(copy)
+    setPoints(copy)
   }
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <button onClick={randomAnecdote}>Random</button>
+      <div>
+        <h3>Anecdote of the day</h3>
+        {props.anecdotes[selected]}
+        <br />
+        <button onClick={randomAnecdote}>Random</button>
+        <button onClick={upvote}>Upvote</button>
+      </div>
+      <div>
+        <h3>Anecdote with the most votes</h3>
+        {props.anecdotes[mostVoteOneIndex]}
+      </div>
     </div>
   )
 }
@@ -19,10 +48,7 @@ const App = (props) => {
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'The first 90 percent of the code accounts for the first 90 percent of the development time.',
 ]
 
 ReactDOM.render(
