a4e6645daf75a7497ba90c824778605172888dde
West Wong <westwong.0622@gmail.com>
2020-07-27 23:13:16 +0800

    show course list

--- Part2/app2/src/App.js
+++ Part2/app2/src/App.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>{course.name}</h1>
       <ul>
-        {notes.map((note) => 
-          <Note key={note.id} note={note} />
-        )}
+        <li>{course.parts[0].name} {course.parts[0].exercises}</li>
+        <li>{course.parts[1].name} {course.parts[1].exercises}</li>
+        <li>{course.parts[2].name} {course.parts[2].exercises}</li>
       </ul>
     </div>
   )
