7bcc7cae69e366e412966cee7fcd891b5cd3b315
West Wong <westwong.0622@gmail.com>
2020-07-29 20:36:09 +0800

    can add number

--- Part2/app2/src/App.js
+++ Part2/app2/src/App.js
@@ -3,24 +3,35 @@ import Person from './components/Person';
 
 const App = (props) => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '17300000000'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
-    if(persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+    if(newName.trim() == '' || newNumber.trim() == ''){
+      alert(`no empty input!`)
     } else{
-      const personObj = {
-        name: newName
+      if(persons.some(person => person.name === newName)) {
+        alert(`${newName} is already added to phonebook`)
+      } else{
+        const personObj = {
+          name: newName,
+          number: newNumber
+        }
+        setPersons(persons.concat(personObj))
+        setNewName('')
+        setNewNumber('')
       }
-      setPersons(persons.concat(personObj))
-      setNewName('')
     }
   }
 
@@ -29,6 +40,7 @@ const App = (props) => {
       <h2>Phonebook</h2>
       <form>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit" onClick={addName}>add</button></div>
       </form>
       <h2>Numbers</h2>
--- Part2/app2/src/components/Person.js
+++ Part2/app2/src/components/Person.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Person = (props) => {
   return (
-    <li>{props.person.name}</li>
+    <li>{props.person.name}: {props.person.number}</li>
   )
 }
 
