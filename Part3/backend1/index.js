62a93b6417e705b997114b5060bd6dd5a6ecf894
West Wong <westwong.0622@gmail.com>
2020-07-31 16:34:01 +0800

    simple server

--- Part3/backend1/index.js
+++ Part3/backend1/index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello Node.js')
+})
+
+const port = 3001
+app.listen(port)
+
+console.log(`Server running on port ${port}`)
