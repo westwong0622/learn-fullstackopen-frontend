e89fd430a5839e8657fee19de93e5d0988884539
West Wong <westwong.0622@gmail.com>
2020-07-31 14:14:33 +0800

    click item to show click event

--- Part2/app2/src/App.js
+++ Part2/app2/src/App.js
@@ -45,6 +45,10 @@ const App = () => {
     setShowAll(!showAll)
   }
 
+  const toggleImportanceOf = (id) => {
+    console.log('importance of ' + id + ' needs to be toggled')
+  }
+
   return (
     <div>
       <h2>Notes</h2>
@@ -55,7 +59,7 @@ const App = () => {
       </div>
       <ul>
         {notes.map(note => 
-          <Note key={note.id} note={note} showAll={showAll} />
+          <Note key={note.id} note={note} showAll={showAll} toggleImportance={() => toggleImportanceOf(note.id)} />
         )}
       </ul>
     </div>
--- Part2/app2/src/components/Note.js
+++ Part2/app2/src/components/Note.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Note = (props) => {
-  if(props.showAll || props.note.important) {
+  if (props.showAll || props.note.important) {
     return (
-      <li>{props.note.id}: {props.note.content}</li>
+      <li>{props.note.id}: {props.note.content}: {props.note.important.toString()} <button onClick={props.toggleImportance}>Change</button></li>
     )
   } else {
     return null
