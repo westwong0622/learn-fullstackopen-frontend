77fb34f70dd469d61ea4f3a4f734d7452ad71075
West Wong <westwong.0622@gmail.com>
2020-07-31 14:55:10 +0800

    show alert when delete none existing note

--- Part2/app2/src/App.js
+++ Part2/app2/src/App.js
@@ -59,6 +59,12 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(note => note.id !== id))
+      })
   }
 
   return (
