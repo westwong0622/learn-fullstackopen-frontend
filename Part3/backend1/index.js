2c0f8dfe0c829bf1162fae4bb5a791f6867473b2
West Wong <westwong.0622@gmail.com>
2020-07-31 17:55:22 +0800

    unknownEndpoint after router

--- Part3/backend1/index.js
+++ Part3/backend1/index.js
@@ -86,6 +86,12 @@ const generateId = () => {
   return maxId + 1
 }
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
