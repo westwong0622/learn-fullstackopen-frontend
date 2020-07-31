5adbe64906abb4fe0566f83ca8d0bb4c194bbcd7
West Wong <westwong.0622@gmail.com>
2020-07-31 17:50:51 +0800

    post a note

--- Part3/backend1/index.js
+++ Part3/backend1/index.js
@@ -49,10 +49,32 @@ app.delete('/api/notes/:id', (req, res) => {
 })
 
 app.post('/api/notes/', (req, res) => {
-  const note = req.body
+  const body = req.body
+
+  if (!body) {
+    return res.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
   console.log(note)
   res.json(note)
 })
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
