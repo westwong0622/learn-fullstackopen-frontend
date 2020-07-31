02b662329db4d92610e72c480e69c6bf772d0478
West Wong <westwong.0622@gmail.com>
2020-07-31 11:50:00 +0800

    axios.post('http://localhost:3001/notes', noteObject)

--- Part2/app2/src/App.js
+++ Part2/app2/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Note from './components/Note';
 
-const App = (props) => {
+const App = () => {
   const [ notes, setNotes ] = useState([])
+  const [ newNote, setNewNote ] = useState('')
 
   const hook = () => {
     console.log('effect')
@@ -17,11 +18,33 @@ const App = (props) => {
 
   useEffect(hook, [])
 
-  console.log('render', notes.length, 'notes')
+  const newNoteChange = (event) => {
+    setNewNote(event.target.value)
+    console.log(event.target.value);
+  }
+
+  const addNewNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
+    }
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response);
+      })
+  }
 
   return (
     <div>
       <h2>Notes</h2>
+      <div>
+        <input value={newNote} onChange={newNoteChange}/>
+        <button onClick={addNewNote}>Add</button>
+      </div>
       <ul>
         {notes.map(note => 
           <Note key={note.id} note={note} />
