cc1114fa88a33283fcc38a6c7e54d65836be7755
West Wong <westwong.0622@gmail.com>
2020-07-31 16:57:30 +0800

    app.get('/api/notes/:id', (req, res) => {})

--- Part3/backend1/index.js
+++ Part3/backend1/index.js
@@ -30,6 +30,14 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  console.log(id);
+  const note = notes.find(note => note.id === id)
+  console.log(note);
+  res.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
