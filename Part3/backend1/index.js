26ff99c134218fee717f4e2ab807260b23638ea3
West Wong <westwong.0622@gmail.com>
2020-08-02 20:35:04 +0800

    link to /api/note with cors

--- Part3/backend1/index.js
+++ Part3/backend1/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
