875bda6e5851a4aabe5e4d2da6f3906d1c2ad297
West Wong <westwong.0622@gmail.com>
2020-08-19 21:12:30 +0800

    fix typo

--- Part2/app2/src/services/login.js
+++ Part2/app2/src/services/login.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl = "/api/login";
 
-const login = async (credentail) => {
+const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials);
   return response.data;
 };
