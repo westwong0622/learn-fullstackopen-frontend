08622c5b213435a44771f2c106b61b78bfcff1ef
West Wong <westwong.0622@gmail.com>
2020-08-20 23:31:16 +0800

    move handler to NoteForm.js

--- Part2/app2/src/components/NoteForm.js
+++ Part2/app2/src/components/NoteForm.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NoteForm = ({ onSubmit, handleChange, value }) => {
+const NoteForm = ({ addNote }) => {
+  const [newNote, setNewNote] = useState("");
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const submitNote = (event) => {
+    event.preventDefault();
+    addNote({
+      content: newNote,
+      date: new Date(),
+      important: Math.random() > 0.5,
+    });
+
+    setNewNote("");
+  };
+
   return (
     <div>
       <h2>Create a new note</h2>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={handleChange} />
+      <form onSubmit={submitNote}>
+        <input value={newNote} onChange={handleChange} />
         <button type="submit">save</button>
       </form>
     </div>
