875d3aa4249607e14c3d159d0923f3601e4dd6a4
West Wong <westwong.0622@gmail.com>
2020-08-24 21:41:49 +0800

    more tests

--- Part2/app2/src/components/Togglable.test.js
+++ Part2/app2/src/components/Togglable.test.js
@@ -17,4 +17,18 @@ describe("<Togglable />", () => {
   test("renders its children", () => {
     expect(component.container.querySelector(".testDiv")).toBeDefined();
   });
+
+  test("at start the children are not displayed", () => {
+    const div = component.container.querySelector(".togglableContent");
+
+    expect(div).toHaveStyle("display: none");
+  });
+
+  test("after clicking the button, children are displayed", () => {
+    const button = component.getByText("show...");
+    fireEvent.click(button);
+
+    const div = component.container.querySelector(".togglableContent");
+    expect(div).not.toHaveStyle("display: none");
+  });
 });
