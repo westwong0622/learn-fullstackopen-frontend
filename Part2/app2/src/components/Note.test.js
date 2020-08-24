b46b9df46194918dbb2e7052f846e04afe4090a9
West Wong <westwong.0622@gmail.com>
2020-08-24 20:45:04 +0800

    fireEvent.click(button);

--- Part2/app2/src/components/Note.test.js
+++ Part2/app2/src/components/Note.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { prettyDOM } from "@testing-library/dom";
 import Note from "./Note";
 
@@ -33,4 +33,19 @@ test("renders content", () => {
   // expect(div).toHaveTextContent(
   //   "Component testing is done with react-testing-library"
   // );
+});
+
+test("clicking the button calls event handler once", () => {
+  const note = {
+    content: "Component testing is done with react-testing-library",
+    important: true,
+  };
+
+  const mockHandler = jest.fn();
+  const component = render(<Note note={note} toggleImportance={mockHandler} />);
+
+  const button = component.getByText("Change");
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
