651494c17a7ed62af91fdea1b1750349a649ca12
West Wong <westwong.0622@gmail.com>
2020-08-25 12:32:57 +0800

    when log in, add new note

--- Part2/app2/cypress/integration/note_app.spec.js
+++ Part2/app2/cypress/integration/note_app.spec.js
@@ -26,4 +26,20 @@ describe("Note app", function () {
 
     cy.contains("Matti Luukkainen logged-in");
   });
+
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.contains("login").click();
+      cy.get("#username").type("mluukkai");
+      cy.get("#password").type("salainen");
+      cy.get("#login-button").click();
+    });
+
+    it("a new note can be created", function () {
+      cy.contains("Add note").click();
+      cy.get("input").type("a note created by cypress");
+      cy.contains("save").click();
+      cy.contains("a note created by cypress");
+    });
+  });
 });
