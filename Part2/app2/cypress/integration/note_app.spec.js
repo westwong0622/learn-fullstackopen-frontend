34ae19549cd49e92447f112c968f90ada9424b30
West Wong <westwong.0622@gmail.com>
2020-08-26 20:36:36 +0800

    test wrong password

--- Part2/app2/cypress/integration/note_app.spec.js
+++ Part2/app2/cypress/integration/note_app.spec.js
@@ -25,6 +25,15 @@ describe("Note app", function () {
     cy.contains("login").click();
   });
 
+  it.only("login fails with wrong password", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("mluukkai");
+    cy.get("#password").type("wrong");
+    cy.get("#login-button").click();
+
+    cy.contains("Wrong credentials");
+  });
+
   it("user can login", function () {
     cy.contains("login").click();
     cy.get("#username").type("mluukkai");
