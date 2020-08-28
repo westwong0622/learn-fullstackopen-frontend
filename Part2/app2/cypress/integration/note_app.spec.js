588bb8b9a44060864f7912a2bf9dc614472a4b10
West Wong <westwong.0622@gmail.com>
2020-08-28 15:14:52 +0800

    use Cypress.Commands.add("createNote" => )

--- Part2/app2/cypress/integration/note_app.spec.js
+++ Part2/app2/cypress/integration/note_app.spec.js
@@ -57,9 +57,10 @@ describe("Note app", function () {
 
     describe("and a note exists", function () {
       beforeEach(function () {
-        cy.contains("Add note").click();
-        cy.get("input").type("another note cypress");
-        cy.contains("save").click();
+        cy.createNote({
+          content: "another note cypress",
+          important: false,
+        });
       });
 
       it("it can be made important", function () {
