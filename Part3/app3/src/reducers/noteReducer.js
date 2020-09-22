84f34f4807eb90eaa4c09fbc0448c88de52500d1
West Wong <westwong.0622@gmail.com>
2020-09-22 22:19:25 +0800

    test("returns new state with action TOGGLE_IMPORTANCE")

--- Part3/app3/src/reducers/noteReducer.js
+++ Part3/app3/src/reducers/noteReducer.js
@@ -1,9 +1,19 @@
 const noteReducer = (state = [], action) => {
-  if (action.type === "NEW_NOTE") {
-    return state.concat(action.data);
+  switch (action.type) {
+    case "NEW_NOTE":
+      return state.concat(action.data);
+    case "TOGGLE_IMPORTANCE": {
+      const id = action.data.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
+    default:
+      return state;
   }
-
-  return state;
 };
 
 export default noteReducer;
