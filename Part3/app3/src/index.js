88a496ff90b3a38a2b84297e68e2deca23c5d2ca
West Wong <westwong.0622@gmail.com>
2020-09-22 21:36:08 +0800

    return state.concat(action.data)

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -5,8 +5,7 @@ import "./index.css";
 
 const noteReducer = (state = [], action) => {
   if (action.type === "NEW_NOTE") {
-    state.push(action.data);
-    return state;
+    return state.concat(action.data);
   }
 
   return state;
