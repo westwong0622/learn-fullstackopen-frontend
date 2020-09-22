a561b160bbc4f8afe414e3a82d88b0824b257d3a
West Wong <westwong.0622@gmail.com>
2020-09-22 21:29:06 +0800

    const counterReducer = (state = 0, action) => { }, const store = createStore(counterReducer)

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -1,12 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
+import { createStore } from "redux";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+const App = () => {
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>
+        Plus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>
+        Minus
+      </button>
+      <button onClick={(e) => store.dispatch({ type: "ZERO" })}>Zero</button>
+    </div>
+  );
+};
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
