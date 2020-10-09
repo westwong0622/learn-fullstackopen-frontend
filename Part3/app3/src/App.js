fa1b23fea75d0e4ea1d77167961bbd05b6aae04b
West Wong <westwong.0622@gmail.com>
2020-10-09 23:05:45 +0800

    add filter

--- Part3/app3/src/App.js
+++ Part3/app3/src/App.js
@@ -3,9 +3,33 @@ import NewNote from "./NewNote";
 import Notes from "./Notes";
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <div>
       <NewNote />
+      <div>
+        all{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("ALL")}
+        />
+        important{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("IMPORTANT")}
+        />
+        nonimportant{" "}
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("NONIMPORTANT")}
+        />
+      </div>
       <Notes />
     </div>
   );
