916c35cbf208c20fe55dcb2a278a65e3d77c2f81
West Wong <westwong.0622@gmail.com>
2020-10-09 21:59:11 +0800

    npm install react-redux

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import noteReducer from "./reducers/noteReducer";
 
@@ -39,7 +40,12 @@ const App = () => {
 };
 
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById("root"));
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
 };
 
 renderApp();
