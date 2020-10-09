c167070fffe87138cd117964bf4054ef4dffa6ae
West Wong <westwong.0622@gmail.com>
2020-10-09 23:06:10 +0800

    import { combineReducers } from "redux";

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer";
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+console.log(store.getState());
 
 const renderApp = () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    // <Provider store={store}>
+    //   <App />
+    // </Provider>,
+    <div />,
     document.getElementById("root")
   );
 };
