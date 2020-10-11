d98833a3b3ed81c38b293720ab99648f862b7297
West Wong <westwong.0622@gmail.com>
2020-10-11 14:50:12 +0800

    move store to store.js

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import noteReducer, { initializeNotes } from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer";
 import noteService from "./services/notes";
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer,
-});
-
-const store = createStore(reducer);
-console.log(store.getState());
+import store from "./store";
 
 noteService.getAll().then((notes) => {
   store.dispatch(initializeNotes(notes));
