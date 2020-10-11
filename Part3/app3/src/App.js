21663bb9f30db536e580fdfafe10fb26e0c22d7f
West Wong <westwong.0622@gmail.com>
2020-10-11 14:33:29 +0800

    useEffect in App.js

--- Part3/app3/src/App.js
+++ Part3/app3/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NewNote from "./NewNote";
 import Notes from "./Notes";
 import VisibilityFilter from "./VisibilityFilter";
+import noteService from "./services/notes";
+import { initializeNotes } from "./reducers/noteReducer";
+import { useDispatch } from "react-redux";
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value);
-  };
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => {
+      dispatch(initializeNotes(notes));
+    });
+  }, [dispatch]);
 
   return (
     <div>
