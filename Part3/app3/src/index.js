96567a605b64d425285e125b5200c3b8b3f7e205
West Wong <westwong.0622@gmail.com>
2020-10-11 14:54:44 +0800

    clear

--- Part3/app3/src/index.js
+++ Part3/app3/src/index.js
@@ -3,13 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
-import noteReducer, { initializeNotes } from "./reducers/noteReducer";
-import noteService from "./services/notes";
 import store from "./store";
-
-noteService.getAll().then((notes) => {
-  store.dispatch(initializeNotes(notes));
-});
 
 const renderApp = () => {
   ReactDOM.render(
