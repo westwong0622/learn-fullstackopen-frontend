e7beb8c05cebd7aa934983f38e2193df6e17bf3c
West Wong <westwong.0622@gmail.com>
2021-02-24 21:07:18 +0800

    clearify

---
+++ Part3/app3/src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleImportanceOf } from "./reducers/noteReducer";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
---
+++ Part3/app3/src/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleImportanceOf } from "./reducers/noteReducer";
+
+const Users = () => {
+  return <div>Users</div>;
+};
+
+export default Users;
