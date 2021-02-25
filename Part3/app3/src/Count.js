3ac366081506afaa114821618870cbe4787608fa
West Wong <westwong.0622@gmail.com>
2021-02-25 23:31:04 +0800

    useHook

--- Part3/app3/src/Count.js
+++ Part3/app3/src/Count.js
@@ -1,15 +1,38 @@
 import React, { useState } from "react";
 const Count = (props) => {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>plus</button>
-      <button onClick={() => setCounter(counter - 1)}>minus</button>
-      <button onClick={() => setCounter(0)}>zero</button>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>plus</button>
+      <button onClick={counter.decrease}>minus</button>
+      <button onClick={counter.zero}>zero</button>
     </div>
   );
+};
+
+const useCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+
+  const decrease = () => {
+    setValue(value - 1);
+  };
+
+  const zero = () => {
+    setValue(0);
+  };
+
+  return {
+    value,
+    increase,
+    decrease,
+    zero,
+  };
 };
 
 export default Count;
