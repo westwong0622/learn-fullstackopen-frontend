f624cdece383204f237316d785dccf7d80177c65
West Wong <westwong.0622@gmail.com>
2021-03-22 23:12:40 +0800

    use jsx

--- Part3/webpack-app/src/index.js
+++ Part3/webpack-app/src/index.js
@@ -1,7 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const hello = (name) => {
-  console.log(`hello ${name}`);
-};
-
-App();
+ReactDOM.render(<App />, document.getElementById("root"));
