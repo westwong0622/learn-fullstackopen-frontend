0edfb0c0ef839a725ec38e254c182afb1b13a016
West Wong <westwong.0622@gmail.com>
2021-03-23 23:18:01 +0800

    add Address, add Person: {} resolver

--- Part4/graphql-backend/main.js
+++ Part4/graphql-backend/main.js
@@ -24,11 +24,15 @@ let persons = [
 ];
 
 const typeDefs = gql`
+  type Address {
+    street: String!
+    city: String!
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String!
+    address: Address!
     id: ID!
   }
 
@@ -44,6 +48,14 @@ const resolvers = {
     personCount: () => persons.length,
     allPersons: () => persons,
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
   },
 };
 
