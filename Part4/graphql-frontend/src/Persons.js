cd526f7c9df027580e4ebb474caf762bc801bb07
West Wong <westwong.0622@gmail.com>
2021-03-29 22:11:33 +0800

    query findPersonByName($nameToSearch: String!) { }

--- Part4/graphql-frontend/src/Persons.js
+++ Part4/graphql-frontend/src/Persons.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      id
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
 
 const Persons = ({ persons }) => {
+  const [person, setPerson] = useState(null);
+  const [getPerson, result] = useLazyQuery(FIND_PERSON);
+
+  const showPerson = (name) => {
+    getPerson({ variables: { nameToSearch: name } });
+  };
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson);
+    }
+  }, [result.data]);
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>
+          {person.address.street} {person.address.city}
+        </div>
+        <div>{person.phone}</div>
+        <button onClick={() => setPerson(null)}>close</button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Persons</h2>
       {persons.map((p) => (
         <div key={p.name}>
           {p.name} {p.phone}
+          <button onClick={() => showPerson(p.name)}>show address</button>
         </div>
       ))}
     </div>
