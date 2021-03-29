227ff3a88d2d1da088ca5f01877b0001e2b8e1be
West Wong <westwong.0622@gmail.com>
2021-03-29 22:28:32 +0800

    update realtime

--- Part4/graphql-frontend/src/components/PersonForm.js
+++ Part4/graphql-frontend/src/components/PersonForm.js
@@ -20,13 +20,25 @@ const CREATE_PERSON = gql`
   }
 `;
 
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+
 const PersonForm = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [street, setStreet] = useState("");
   const [city, setCity] = useState("");
 
-  const [createPerson] = useMutation(CREATE_PERSON);
+  const [createPerson] = useMutation(CREATE_PERSON, {
+    refetchQueries: [{ query: ALL_PERSONS }],
+  });
 
   const submit = (event) => {
     event.preventDefault();
