ea0167437eec250fd2176dd5bbfbc7234b008299
West Wong <westwong.0622@gmail.com>
2021-03-29 20:52:45 +0800

    add client.query

--- Part4/graphql-frontend/src/index.js
+++ Part4/graphql-frontend/src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000",
+  }),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
