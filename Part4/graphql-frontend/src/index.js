b0657d793e6e2b8314c1a778b7d101bc3d36e0ec
West Wong <westwong.0622@gmail.com>
2021-04-07 22:59:20 +0800

    import { setContext } from "apollo-link-context";

--- Part4/graphql-frontend/src/index.js
+++ Part4/graphql-frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { setContext } from "apollo-link-context";
+
 import {
   ApolloClient,
   ApolloProvider,
@@ -12,11 +14,21 @@ import {
   gql,
 } from "@apollo/client";
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("phonenumbers-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "http://localhost:4000",
-  }),
+  link: authLink.concat(httpLink),
 });
 
 const query = gql`
