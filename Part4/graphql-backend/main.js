e8021b296fb9ff343d68dc4deaceb3a45cb82477
West Wong <westwong.0622@gmail.com>
2021-04-08 09:33:53 +0800

    add subscriptionsUrl in server.listen

--- Part4/graphql-backend/main.js
+++ Part4/graphql-backend/main.js
@@ -226,6 +226,7 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`Server ready at ${url}`);
+  console.log(`Subscriptions ready at ${subscriptionsUrl}`);
 });
