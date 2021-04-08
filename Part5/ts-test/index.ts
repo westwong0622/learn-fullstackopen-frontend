9c8a83e8ee0c038bd9e5c758b5689fee1af46d68
West Wong <westwong.0622@gmail.com>
2021-04-08 14:38:45 +0800

    require to import, req to _req

--- Part5/ts-test/index.ts
+++ Part5/ts-test/index.ts
@@ -1,7 +1,7 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
