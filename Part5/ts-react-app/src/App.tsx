3585ad50439cb9d5fcfc6c2cc6433b84cd4e664e
West Wong <westwong.0622@gmail.com>
2021-04-08 23:22:11 +0800

    type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;

--- Part5/ts-react-app/src/App.tsx
+++ Part5/ts-react-app/src/App.tsx
@@ -5,6 +5,30 @@ import Header from './comp/Header';
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+  
+  interface CourseNormalPart extends CoursePartBase {
+    type: "normal";
+    description: string;
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+  
+  interface CourseSubmissionPart extends CoursePartBase {
+    type: "submission";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+  
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+  
   const courseParts = [
     {
       name: "Fundamentals",
