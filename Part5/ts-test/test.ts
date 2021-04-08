1246c8a2ddc08baf48df611b45e7b75c82fa5255
West Wong <westwong.0622@gmail.com>
2021-04-08 14:03:41 +0800

    type Operation

--- Part5/ts-test/test.ts
+++ Part5/ts-test/test.ts
@@ -3,3 +3,19 @@ const multiplicator = (a:number, b:number, printText:string) => {
 }
 
 multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op : Operation) => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide by 0!';
+    return a / b;
+  }
+}
+
+const result = calculator(1, 2, "add");
+console.log(result);
