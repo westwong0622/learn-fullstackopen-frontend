871f585daceb632e2db1205444a4b246b347c3be
West Wong <westwong.0622@gmail.com>
2021-04-09 00:16:55 +0800

    use list in Content

--- Part5/ts-react-app/src/comp/Content.tsx
+++ Part5/ts-react-app/src/comp/Content.tsx
@@ -7,17 +7,17 @@ interface ContentProps {
 }
 
 const Content = (props: ContentProps) => {
+  const listItems = props.courseParts.map((part) => {
+    return (
+      <li>
+        {part.name} {part.exerciseCount}
+      </li>
+    );
+  });
+
   return (
     <div>
-      <p>
-        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
-      </p>
+      <ul>{listItems}</ul>
     </div>
   );
 };
