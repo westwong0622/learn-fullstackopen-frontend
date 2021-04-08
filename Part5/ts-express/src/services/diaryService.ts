ccdb7779a4d818f7e2fcd0384124ee8fba5c373e
West Wong <westwong.0622@gmail.com>
2021-04-08 16:34:22 +0800

    add diaryService.js

---
+++ Part5/ts-express/src/services/diaryService.ts
@@ -0,0 +1,14 @@
+import diaryData from '../../diaryentries.json';
+
+const getEntries = () => {
+  return diaryData;
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addEntry
+};
