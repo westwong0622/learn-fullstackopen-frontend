9ebd54e41d3a5727265687dbe33ce85247da850e
West Wong <westwong.0622@gmail.com>
2021-04-08 14:09:11 +0800

    use throw error to handle divided by 0 problem

--- Part5/ts-test/test.ts
+++ Part5/ts-test/test.ts
@@ -5,18 +5,26 @@ const multiplicator = (a: number, b: number, printText: string) => {
 multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
 
 type Operation = 'multiply' | 'add' | 'divide';
-type Result = string | number;
+type Result = number;
 
 const calculator = (a: number, b: number, op : Operation): Result => {
-  if (op === 'multiply') {
-    return a * b;
-  } else if (op === 'add') {
-    return a + b;
-  } else if (op === 'divide') {
-    if (b === 0) return 'can\'t divide by 0!';
-    return a / b;
+  switch(op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if( b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!');
   }
 }
 
-const result = calculator(1, 2, "add");
-console.log(result);
+try {
+  console.log(calculator(1, 4 , 'divide'))
+  console.log(calculator(0, 4 , 'divide'))
+  console.log(calculator(4, 0 , 'divide'))
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
