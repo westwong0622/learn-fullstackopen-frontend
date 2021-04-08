c093c60f4ea45b24f8b38ab14282d386a1117f1b
West Wong <westwong.0622@gmail.com>
2021-04-08 09:51:40 +0800

    updateCacheWith

--- Part4/graphql-frontend/src/App.js
+++ Part4/graphql-frontend/src/App.js
@@ -4,11 +4,31 @@ import PersonForm from "./components/PersonForm";
 import PhoneForm from "./components/PhoneForm";
 import Notify from "./components/Notify";
 import LoginForm from "./components/LoginForm";
-import { useQuery, useApolloClient } from "@apollo/client";
+import { useQuery, useApolloClient, useSubscription } from "@apollo/client";
 
-import { ALL_PERSONS } from "./service/queries.js";
+import { ALL_PERSONS, PERSON_ADDED } from "./service/queries.js";
 
 const App = () => {
+  const updateCacheWith = (addedPerson) => {
+    const includedIn = (set, object) =>
+      set.map((p) => p.id).includes(object.id);
+
+    const dataInStore = client.readQuery({ query: ALL_PERSONS });
+    if (!includedIn(dataInStore.allPersons, addedPerson)) {
+      client.writeQuery({
+        query: ALL_PERSONS,
+        data: { allPersons: dataInStore.allPersons.concat(addedPerson) },
+      });
+    }
+  };
+  useSubscription(PERSON_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      const addedPerson = subscriptionData.data.personAdded;
+      notify(`${addedPerson.name} added`);
+      updateCacheWith(addedPerson);
+    },
+  });
+
   const result = useQuery(ALL_PERSONS);
   const [errorMessage, setErrorMessage] = useState(null);
   const [token, setToken] = useState(null);
