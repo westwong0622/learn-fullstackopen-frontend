b4bc83cc0f194185f1845f08e81ea8e1132aaae8
West Wong <westwong.0622@gmail.com>
2021-04-08 23:05:22 +0800

    add more props for courseParts

--- Part5/ts-react-app/src/App.tsx
+++ Part5/ts-react-app/src/App.tsx
@@ -8,15 +8,19 @@ const App: React.FC = () => {
   const courseParts = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
     }
   ];
 
