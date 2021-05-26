3ed8b6e38832b1418426e3f4790b6e206f7938aa
West Wong <westwong.0622@gmail.com>
2021-05-26 23:00:58 +0800

    use my Text in Main

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Constants from "expo-constants";
 import { Text, StyleSheet, View } from "react-native";
 import RepositoryList from "./RepositoryList";
+import MyText from "./Text";
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +16,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <Text>Rate Repository Application</Text>
+      <MyText>Simple text</MyText>
+      <MyText style={{ paddingBottom: 10 }}>Text with custom style</MyText>
+      <MyText fontWeight="bold" fontSize="subheading">
+        Bold subheading
+      </MyText>
       <RepositoryList />
     </View>
   );
