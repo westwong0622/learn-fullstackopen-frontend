a11c08f597accddbb1e9f0f81515c7838dd3f5b0
West Wong <westwong.0622@gmail.com>
2021-05-26 22:52:59 +0800

    <Text style={styles.text}>

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  id: {
+    color: "blue",
+    fontSize: 24,
+    fontWeight: "700",
+  },
+});
 
 const RepositoryItem = (props) => {
   return (
     <View>
-      <Text>{props.itemData.id}</Text>
+      <Text style={styles.id}>{props.itemData.id}</Text>
       <Text>{props.itemData.fullName}</Text>
       <Text>{props.itemData.description}</Text>
     </View>
